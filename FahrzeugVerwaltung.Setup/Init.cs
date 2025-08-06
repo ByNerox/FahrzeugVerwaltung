15a9fe750b00448a8da790aeab1d713fd4eb57b5
e.bewig <e.bewig@uni-hildesheim.de>
2025-08-06 08:54:08 +0200

    Dependency Injection Unity

---
+++ FahrzeugVerwaltung.Setup/Init.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FahrzeugVerwaltung;
+using FahrzeugVerwaltung.Service;
+using Unity;
+
+namespace FahrzeugVerwaltung.Setup
+{
+    internal class Init
+    {
+        public static IUnityContainer container { get; private set; }
+    
+
+    public static void Initialize()
+        {
+            container = new UnityContainer();
+
+            
+            container.RegisterType<IVehicleService, VehicleService>();
+        }
+    }
+}
---
+++ FahrzeugVerwaltung.Setup/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aufgabe;
+using Unity;
+
+namespace FahrzeugVerwaltung.Setup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Init.Initialize();
+
+            IVehicleService vehicleService = Init.container.Resolve<IVehicleService>();
+
+            IEnumerable<Vehicle> allVehicles = vehicleService.GetAll();
+
+            foreach (Vehicle vehicle in allVehicles)
+            {
+                Console.WriteLine($"ID: {vehicle.Ident} | {vehicle.Type} | {vehicle.Brand} {vehicle.Model}");
+                if (vehicle is LKW lkw)
+                {
+                    Console.WriteLine($"-> Kapazität: {lkw.Capacity} Tonnen");
+                }
+            }
+        }
+    }
+}
